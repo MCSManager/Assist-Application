1c1de1fc8d8b7a942f9c04303eb0b2943c005147
zijiren <pyh1670605849@gmail.com>
2022-06-26 22:27:33 +0800

    更新unzip.go 添加utf8、gbk的识别

--- utils/unzip.go
+++ utils/unzip.go
@@ -1,4 +1,4 @@
-package utils
+package zip
 
 import (
 	"archive/zip"
@@ -21,15 +21,17 @@ func Unzip(zipPath string, targetPath string) error {
 	defer zipReader.Close()
 	var decodeName string
 	for _, f := range zipReader.File {
-		if f.Flags == 0 {
+		// fmt.Printf("isUtf8([]byte(f.Name)): %v\n", isUtf8([]byte(f.Name)))
+		// fmt.Printf("isGBK([]byte(f.Name)): %v\n", isGBK([]byte(f.Name)))
+		if isUtf8([]byte(f.Name)) {
+			decodeName = f.Name
+		} else if isGBK([]byte(f.Name)) {
 			i := bytes.NewReader([]byte(f.Name))
 			decoder := transform.NewReader(i, simplifiedchinese.GB18030.NewDecoder())
+			// decoder := transform.NewReader(i, traditionalchinese.Big5.NewDecoder())
 			content, _ := ioutil.ReadAll(decoder)
 			decodeName = string(content)
-		} else {
-			decodeName = f.Name
 		}
-
 		fpath := filepath.Join(targetPath, decodeName)
 		if f.FileInfo().IsDir() {
 			os.MkdirAll(fpath, os.ModePerm)
@@ -54,4 +56,67 @@ func Unzip(zipPath string, targetPath string) error {
 		}
 	}
 	return nil
+}
+
+// 先判断是否是UTF8再判断是否是其它编码才有意义
+func isUtf8(data []byte) bool {
+	i := 0
+	for i < len(data) {
+		if (data[i] & 0x80) == 0x00 {
+			i++
+			continue
+		} else if num := preNUm(data[i]); num > 2 {
+			i++
+			for j := 0; j < num-1; j++ {
+				//判断后面的 num - 1 个字节是不是都是10开头
+				if (data[i] & 0xc0) != 0x80 {
+					return false
+				}
+				i++
+			}
+		} else {
+			//其他情况说明不是utf-8
+			return false
+		}
+	}
+	return true
+}
+
+func isGBK(data []byte) bool {
+	length := len(data)
+	var i int = 0
+	for i < length {
+		if data[i] <= 0x7f {
+			//编码0~127,只有一个字节的编码，兼容ASCII码
+			i++
+			continue
+		} else {
+			//大于127的使用双字节编码，落在gbk编码范围内的字符
+			if data[i] >= 0x81 &&
+				data[i] <= 0xfe &&
+				data[i+1] >= 0x40 &&
+				data[i+1] <= 0xfe &&
+				data[i+1] != 0x7f {
+				i += 2
+				continue
+			} else {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func preNUm(data byte) int {
+	var mask byte = 0x80
+	var num int = 0
+	for i := 0; i < 8; i++ {
+		if (data & mask) == mask {
+			num++
+			mask = mask >> 1
+		} else {
+			break
+		}
+	}
+	return num
 }
